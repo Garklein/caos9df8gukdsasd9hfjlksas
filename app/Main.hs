f08b1bb5507ea186aa2a75c76c26361ae4683c65
Garklein <garklein97@gmail.com>
2023-06-15 16:52:47 -0400

    better hakil

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LexicalNegation #-}
 module Main where
 
 import Data.Bool
-import Data.List
 import Codec.BMP
-import Data.Maybe
+import Data.Function
 import Graphics.Gloss
+import Data.Traversable
 import System.Directory
+import Data.Map qualified as M
 import Data.Vector qualified as V
 import Graphics.Gloss.Interface.IO.Interact
 
@@ -22,28 +24,23 @@ data Chessman = Pawn
               | Bishop
               | Queen
               | King
-letter :: Chessman -> String
-letter Pawn   = "p"
-letter Rook   = "r"
-letter Horsey = "n"
-letter Bishop = "b"
-letter Queen  = "q"
-letter King   = "k"
-
+  deriving (Eq, Ord)
 data Piece = White Chessman | Black Chessman | Empty
-pieceName :: Piece -> String
-pieceName (Black x) = letter x ++ "d"
-pieceName (White x) = letter x ++ "l"
-pieceName Empty     = ""
+  deriving (Eq, Ord)
+
+--instance Eq Piece where
+--  Empty   == Empty    =  True
+--  White x == White y  =  x == y
+--  Black x == Black y  =  x == y
+--instance Ord Piece where
+--  compare (White x) (White y) = compare x y
+--  compare (White _) _ = GT
+--  compare (Black x) (Black y) = compare x y
+--  compare (Black kk
+--  compare Empty Empty = EQ
+--  compare _ _ = EQ
 
 type Board = V.Vector (V.Vector Piece)
-
-imgNames :: [String]
-imgNames = pieces "d" ++ pieces "l"
-  where
-    pieces c = (++ c) <$> ["k", "q", "r", "n", "b", "p"]
-
-
 
 q :: Float -- half the width of the board
 q = 300
@@ -51,23 +48,23 @@ q = 300
 square :: Float -> Float -> Float -> Path
 square x y w = [(x, y), (x+w, y), (x+w, y+w), (x, y+w), (x, y)]
 
-drawBoard :: [Picture] -> Board -> Picture
-drawBoard imgs board = Pictures $ liftA2 all [0..7] [0..7]
+drawBoard :: M.Map Piece Picture -> Board -> Picture
+drawBoard imgs board = Pictures $ liftA2 draw [0..7] [0..7]
   where
-    all :: Int -> Int -> Picture
-    all    x y        = Pictures $ (\f -> f x y (fromIntegral x * w - q) (fromIntegral y * w - q)) <$> [tile, img]
-    img    x y cX cY  = Translate (cX + w/2) (cY + w/2) $ getImg x y
+    draw :: Int -> Int -> Picture
+    draw   x y        = Pictures $ (\f -> f x y (coord x) (coord y)) <$> [tile, img]
+    coord             = (- q) . (* (q/4)) . fromIntegral
+    img    x y cX cY  = Translate (cX + q/8) (cY + q/8) $ getImg x y
     getImg x y        = case board V.! y V.! x of
                           Empty -> Pictures []
-                          p     -> imgs !! fromJust (elemIndex (pieceName p) imgNames)
-    tile   x y cX cY  = colour x y . polygon $ square cX cY w
+                          p     -> imgs M.! p
+    tile   x y cX cY  = colour x y . polygon $ square cX cY (q/4)
     colour            = Color . bool white (light $ light blue) . odd . fromEnum .: (+)
-    w                 = q / 4
 
 border :: Picture
 border = Line $ square -q -q (q*2)
 
-convert :: [Picture] -> Board -> Picture
+convert :: M.Map Piece Picture -> Board -> Picture
 convert imgs board = Pictures [drawBoard imgs board, border]
 
 events :: Event -> Board -> Board
@@ -82,25 +79,31 @@ home = V.fromList [Rook, Horsey, Bishop, Queen, King, Bishop, Horsey, Rook]
 start :: Board
 start = V.fromList
   [ Black <$> home
-  , gimme8 $ Black Pawn
-  , gimme8 Empty
-  , gimme8 Empty
-  , gimme8 Empty
-  , gimme8 Empty
-  , gimme8 $ White Pawn
+  , V.replicate 8 $ Black Pawn
+  , empty
+  , empty
+  , empty
+  , empty
+  , V.replicate 8 $ White Pawn
   , White <$> home
   ]
-  where gimme8 = V.replicate 8
+  where empty = V.replicate 8 Empty
+
+imgNames :: [String]
+imgNames = liftA2 (\p c -> [p, c]) "kqrnbp" "dl"
+
+pieceOrder :: [Piece]
+pieceOrder = liftA2 (&) [King, Queen, Rook, Horsey, Bishop, Pawn] [Black, White]
+
 
 main :: IO ()
 main = do
-  path <- (++ "/imgs/") <$> getCurrentDirectory
-  let getImg name = loadBMP $ path ++ name ++ ".bmp"
-  imgs <- traverse getImg imgNames
+  path <- (<> "/imgs/") <$> getCurrentDirectory
+  imgs <- for imgNames $ \name -> loadBMP $ path <> name <> ".bmp"
+  Right bmp <- readBMP $ path <> "kd.bmp"
 
-  Right bmp <- readBMP $ path ++ "kd.bmp"
-  let factor = (q / 4 /) . fromIntegral . fst $ bmpDimensions bmp -- it's a square.
-  let scaledImgs = (Scale factor factor) <$> imgs
+  let factor = q/4 / (fromIntegral . fst $ bmpDimensions bmp) -- it's a square.
+      scaledImgs = Scale factor factor <$> imgs
+      window = InWindow "caos9df8gukdsasd9hfjlksas!!!" (200, 200) (10, 10)
 
-  let window = InWindow "caos9df8gukdsasd9hfjlksas!!!" (200, 200) (10, 10)
-  play window white 0 start (convert scaledImgs) events step
+  play window white 0 start (convert . M.fromList $ zip pieceOrder scaledImgs) events step
